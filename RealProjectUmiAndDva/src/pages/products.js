9322310cff545cc1489d950a58d56eb5b841b212
duynguyen19051997 <nguyenvuanhduy1905@gmail.com>
2020-04-04 17:39:44 +0700

    Antd-Demo

--- RealProjectUmiAndDva/src/pages/products.js
+++ RealProjectUmiAndDva/src/pages/products.js
@@ -2,6 +2,7 @@ import { connect } from 'umi';
 import ProductList from '@/components/ProductList';
 
 const Products = ({ dispatch, products }) => {
+
   function handleDelete(id) {
     dispatch({
       type: 'products/delete',
@@ -11,7 +12,7 @@ const Products = ({ dispatch, products }) => {
   return (
     <div>
       <h2>List of Products</h2>
-      <ProductList onDelete={handleDelete} products={products} />
+      <ProductList onDelete={handleDelete} okTest={"Yes"} cancelText={"No"} products={products} />
     </div>
   );
 };
